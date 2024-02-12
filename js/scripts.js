3e640312873fbc12457256bea8c368486edcda2b
Matheus <matheus.mmn@hotmail.com>
2024-02-12 11:15:19 -0300

    protótipo beta de uma chamada virtual

--- js/scripts.js
+++ js/scripts.js
@@ -10,68 +10,25 @@ class Calculator {
   }
 
   addDigit(digit) {
-    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
-      return;
-    }
-
     this.currentOperation = digit;
     this.updateScreen();
   }
 
-  // process all calculator operations
   processOperation(operation) {
-    
-    // Check if current value is empty
-    if (this.currentOperationText.innerText === "" && operation !== "C") {
-      // Change operation
-      if (this.previousOperationText.innerText !== "") {
-        this.changeOperation(operation);
-      }
-      return;
-    }
-
-    // Get current and previous values
-    let operationValue;
-    let previous = +this.previousOperationText.innerText.split(" ")[0];
-    let current = +this.currentOperationText.innerText;
-    
-
     switch (operation) {
-      case "+":
-        operationValue = previous + current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
-      case "-":
-        operationValue = previous - current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
-      case "*":
-        operationValue = previous * current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
-      case "/":
-        operationValue = previous / current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
       case "_":
         this.processDelOperator();
-        break;
-      case "CE":
-        this.processClearCurrentOperator();
-        break;
-      case "C":
-        this.processClearOperator();
         break;
       case ">":
         // this.processEqualOperator();
         alert("Presença confirmada")
+        this.currentOperationText.innerText = "";
         break;
       default:
         return;
     }
   }
 
-  // Change values of calculator screen
   updateScreen(
     operationValue = null,
     operation = null,
@@ -79,52 +36,23 @@ class Calculator {
     previous = null
   ) {
     if (operationValue === null) {
-      // Append number to current value
       this.currentOperationText.innerText += this.currentOperation;
     } else {
-      // Check if value is zero, if is just add current value
       if (previous === 0) {
         operationValue = current;
       }
-      // Add current value to previous
       this.previousOperationText.innerText = `${operationValue} ${operation}`;
       this.currentOperationText.innerText = "";
     }
   }
 
-  // Change math operation
-  changeOperation(operation) {
-    const mathOperations = ["*", "-", "+", "/"];
-
-    if (!mathOperations.includes(operation)) {
-      return;
-    }
-
-    this.previousOperationText.innerText =
-      this.previousOperationText.innerText.slice(0, -1) + operation;
-  }
-
-  // Delete a digit
   processDelOperator() {
     this.currentOperationText.innerText =
       this.currentOperationText.innerText.slice(0, -1);
   }
 
-  // Clear current operation
-  processClearCurrentOperator() {
-    this.currentOperationText.innerText = "";
-  }
-
-  // Clear all operations
-  processClearOperator() {
-    this.currentOperationText.innerText = "";
-    this.previousOperationText.innerText = "";
-  }
-
-  // Process an operation
   processEqualOperator() {
     let operation = this.previousOperationText.innerText.split(" ")[1];
-
     this.processOperation(operation);
   }
 }
@@ -136,7 +64,6 @@ buttons.forEach((btn) => {
     const value = e.target.innerText;
 
     if (+value >= 0 || value === ".") {
-      console.log(value);
       calc.addDigit(value);
     } else {
       calc.processOperation(value);
